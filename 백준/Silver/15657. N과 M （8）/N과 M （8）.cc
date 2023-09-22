fb7a2c02d485106b799fb0f31e6845a8e581c68c
Kim Yuna <45454585+yunakim2@users.noreply.github.com>
2023-09-22 14:47:18 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int checking[10];
+vector <int> number;
+int N, M;
+
+void go(int start, int cnt) {
+  if (cnt == M) {
+    for (int i = 0; i < M; i++) {
+      cout << number[checking[i]] << " ";
+    }
+    cout << "\n";
+    return;
+  }
+
+  for (int i = start; i < N; i++) {
+    checking[cnt] = i;
+    go(i, cnt + 1);
+  }
+}
+
+int main() {
+
+  cin >> N >> M;
+  for (int i = 0; i < N; i++) {
+    int num;
+    cin >> num;
+    number.push_back(num);
+  }
+  sort(number.begin(), number.end());
+  go(0, 0);
+  return 0;
+}
